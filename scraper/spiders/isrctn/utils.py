39b9e95bd90bb90588c726e9a3b10c7615dd092a
roll <roll@post.agency>
2016-01-19 11:10:41 +0300

    cleaned isrctn.utils

--- scraper/spiders/isrctn/utils.py
+++ scraper/spiders/isrctn/utils.py
@@ -4,13 +4,8 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import json
-import logging
-import xmltodict
 from urllib import urlencode
 from collections import OrderedDict
-
-logger = logging.getLogger(__name__)
 
 
 def make_start_urls(base, date_from, date_to):
@@ -34,55 +29,3 @@ def make_pattern(base):
     pattern += r'\?q=&filters=GT[^,]*,LE[^,]*&page=\d+&'
     pattern += 'pageSize=100&searchType=advanced-search$'
     return pattern
-
-
-def get_text(res, path, process=None):
-    """Extract text from response by path.
-    """
-    value = None
-    try:
-        nodes = res.xpath(path)
-        if nodes:
-            value = nodes.xpath('text()').extract_first()
-            if process:
-                value = process(value)
-    except Exception as exception:
-        logger.debug(path + ': ' + str(exception))
-    return value
-
-
-def get_dict(res, path, expand=None):
-    """Extract dict from response by path.
-    """
-    value = None
-    try:
-        nodes = res.xpath(path)
-        if nodes:
-            text = nodes.extract_first()
-            hash = xmltodict.parse(text)
-            if expand:
-                hash = hash[expand]
-            value = json.dumps(hash)
-    except Exception as exception:
-        logger.debug(path + ': ' + str(exception))
-    return value
-
-
-def get_list(res, path, expand=None):
-    """Extract list from response by path.
-    """
-    value = None
-    try:
-        nodes = res.xpath(path)
-        if nodes:
-            hashs = []
-            texts = nodes.extract()
-            for text in texts:
-                hash = xmltodict.parse(text)
-                if expand:
-                    hash = hash[expand]
-                hashs.append(hash)
-            value = json.dumps(hashs)
-    except Exception as exception:
-        logger.debug(path + ': ' + str(exception))
-    return value
