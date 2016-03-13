f3e7db671e7036672ab3d7acc33baba88e772eda
roll <roll@post.agency>
2016-03-13 23:54:13 +0300

    min [delpoy]

--- migrations/versions/20160311151047_update_meta_identifier.py
+++ migrations/versions/20160311151047_update_meta_identifier.py
@@ -9,7 +9,6 @@ from sqlalchemy.dialects.postgresql import ARRAY, JSONB, UUID
 from alembic import op
 
 
-
 # revision identifiers, used by Alembic.
 revision = '46d169ce43d2'
 down_revision = u'b0f8a397edad'
