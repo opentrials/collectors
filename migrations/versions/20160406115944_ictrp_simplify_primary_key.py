e95e98ffedeaabb2c66075bd58a8430ed34e641a
roll <roll@post.agency>
2016-04-06 12:05:05 +0300

    removed register from ictrp primary_key

---
+++ migrations/versions/20160406115944_ictrp_simplify_primary_key.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import ARRAY, JSONB
+from alembic import op
+
+
+
+# revision identifiers, used by Alembic.
+revision = '00d329f5f40a'
+down_revision = u'58d2189bc678'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_constraint('ictrp_pkey', 'ictrp')
+    op.create_primary_key('ictrp_pkey', 'ictrp', ['main_id'])
+
+
+def downgrade():
+    op.drop_constraint('ictrp_pkey', 'ictrp')
+    op.create_primary_key('ictrp_pkey', 'ictrp', ['register', 'main_id'])
