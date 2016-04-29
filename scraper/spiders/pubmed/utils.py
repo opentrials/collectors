15d4cebaccb69c8685c2f1913e8bf1d33ee3245d
roll <roll@post.agency>
2016-04-29 12:54:33 +0200

    updated search term fo pubmed

--- scraper/spiders/pubmed/utils.py
+++ scraper/spiders/pubmed/utils.py
@@ -18,32 +18,32 @@ logger = logging.getLogger(__name__)
 def make_start_urls(prefix, template, date_from=None, date_to=None):
     """ Return start_urls.
     """
+
+    # Prepare dates
     if date_from is None:
         date_from = str(date.today() - timedelta(days=1))
     if date_to is None:
         date_to = str(date.today())
     date_from = datetime.strptime(date_from, '%Y-%m-%d').strftime('%Y/%m/%d')
     date_to = datetime.strptime(date_to, '%Y-%m-%d').strftime('%Y/%m/%d')
+
+    # Prepare query
     query = OrderedDict()
     query['db'] = 'pubmed'
+    query['term'] = 'trial registration'  # TODO: review
     query['retmode'] = 'json'
     query['datetype'] = 'pdat'  # TODO: review
     query['mindate'] = date_from
     query['maxdate'] = date_to
+    query['retmax'] = '100000'
+
+    # Make request
+    res = requests.get(prefix + '?' + urlencode(query))
+    pmids = res.json()['esearchresult']['idlist']
     urls = []
-    retstart = 0
-    retmax = 100000
-    while True:
-        query['retstart'] = retstart
-        query['retmax'] = retmax
-        res = requests.get(prefix + '?' + urlencode(query))
-        pmids = res.json()['esearchresult']['idlist']
-        if not pmids:
-            break
-        for pmid in pmids:
-            urls.append(template.format(pmid=pmid))
-        logger.debug('Populating Pubmed start urls: %s added', len(urls))
-        retstart += retmax
+    for pmid in pmids:
+        urls.append(template.format(pmid=pmid))
+    logger.debug('Populated Pubmed start urls: %s', len(urls))
     return urls
 
 
