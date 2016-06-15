653eb5c6a982be81b18e25b17aeb572c7efdb9b6
roll <roll@post.agency>
2016-06-15 09:47:09 +0300

    fixed test warehouse url in config

--- collectors/base/config.py
+++ collectors/base/config.py
@@ -40,7 +40,7 @@ AUTOTHROTTLE_ENABLED = True
 # Pipelines
 
 WAREHOUSE_URL = os.environ['WAREHOUSE_URL']
-TEST_WAREHOUSE_URL = os.environ['TEST_WAREHOUSE_URL']
+TEST_WAREHOUSE_URL = os.environ.get('TEST_WAREHOUSE_URL', None)
 ITEM_PIPELINES = {
     'collectors.base.pipelines.Warehouse': 100,
 }
