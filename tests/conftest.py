4a11514cf5622eed6092958db2aecdc7a42060d8
Vitor Baptista <vitor@vitorbaptista.com>
2016-07-18 14:24:19 +0100

    [#263] Remove custom "betamax_session" fixture
    Betamax itself already provides the same fixture with the same name.
    Coincidences :P

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,6 @@ from __future__ import unicode_literals
 import os
 import pytest
 import betamax
-import requests
 import dataset
 from scrapy.http import Request, HtmlResponse
 from collectors.base import config, helpers
@@ -39,22 +38,6 @@ def conn():
     for table in warehouse.tables:
         warehouse[table].delete()
     return {'warehouse': warehouse}
-
-
-@pytest.yield_fixture
-def betamax_session(request):
-    '''Setups a betamax cassette and yields a requests session using it.
-
-    The cassette name is generated based on the calling class and method names.
-    '''
-    class_name = request.instance.__class__.__name__
-    method_name = request.function.__name__
-    cassette_name = '.'.join([class_name, method_name])
-    session = requests.Session()
-    recorder = betamax.Betamax(session)
-
-    with recorder.use_cassette(cassette_name):
-        yield session
 
 
 @pytest.fixture
